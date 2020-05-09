02b5461bcb00e94152c57e423792cb7d94e4ac92
Luke Malcolm <email@lukemalcolm.com>
2020-05-10 02:42:36 +1200

    Add helper script for copying built files into place.

---
+++ deployDebug.cmd
@@ -0,0 +1,3 @@
+XCOPY .\Sources\Windows\x64\Debug\notchStreamDeck.exe "C:\Program Files\Elgato\StreamDeck\Plugins\com.elgato.notch.sdPlugin" /Y
+
+XCOPY .\Sources\com.elgato.notch.sdPlugin\*.* "C:\Program Files\Elgato\StreamDeck\Plugins\com.elgato.notch.sdPlugin" /E /D /C /Y
